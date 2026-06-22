f2bce40f3045c15a12937711c07e080da69a0349
DavidDeutz <deutz.david@gmx.at>
2026-06-22 14:31:07 +0200

    added first version experiments and analysis

---
+++ experiments/download_dataset.py
@@ -0,0 +1,45 @@
+import os
+
+import fiftyone as fo
+import fiftyone.zoo as foz
+
+EXPORT_DIR = "./experiments/dataset"
+LABEL_FIELD = "ground_truth"
+DATASET_TYPE = fo.types.COCODetectionDataset
+CLASSES = ["dog", "cat", "bird", "car", "bicycle", "chair", "apple"]
+
+dataset = foz.load_zoo_dataset(
+    "coco-2017",
+    split="validation",
+    classes=CLASSES,
+    max_samples=1000,
+)
+
+# ----------
+# Create Single object data
+# ----------
+single_object_view = dataset.match(
+    fo.ViewField("ground_truth.detections").length() <= 2
+)
+for cls in CLASSES:
+    view = single_object_view.filter_labels(
+        LABEL_FIELD, fo.ViewField("label") == cls
+    ).take(10)
+
+    view.export(
+        export_dir=os.path.join(EXPORT_DIR, "single_object", cls),
+        dataset_type=DATASET_TYPE,
+        label_field=LABEL_FIELD,
+    )
+
+# ----------
+# Create Multi object data
+# ----------
+multi_object_view = dataset.match(
+    fo.ViewField("ground_truth.detections").length() >= 3
+).take(30)
+multi_object_view.export(
+    export_dir=os.path.join(EXPORT_DIR, "multi_object"),
+    dataset_type=DATASET_TYPE,
+    label_field=LABEL_FIELD,
+)
